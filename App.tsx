9e1fa730d42e1b3b9c509ba2e74c975e4d6d3821
chan1992241 <chan1992241@gmail.com>
2023-10-14 21:51:39 +0800

    Initialize MUI

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './app/Home';
@@ -8,12 +11,16 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="RequestCarPool" component={RequestCarPool} />
-        <Stack.Screen name="OfferCarPool" component={OfferCarPool} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="RequestCarPool" component={RequestCarPool} />
+          <Stack.Screen name="OfferCarPool" component={OfferCarPool} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
+
+AppRegistry.registerComponent('App', () => App);
